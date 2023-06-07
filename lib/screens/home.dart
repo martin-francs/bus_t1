88cfbf2ea09280e1e7bcd4964ba33a7e12befe26
Martin Francis Paul <francismartin0078@gmail.com>
2023-06-08 01:33:46 +0530

    wallet ui component added to home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -52,14 +52,72 @@ String result = "Hello World...!";
 }
   @override
   Widget build(BuildContext context) {
+    String bal="1";
     return Scaffold(
       backgroundColor:Colors.white,
-      body:SafeArea(child: Column(children:[Column(children:[ Text('Welcome home,\n'+fname,style: TextStyle(fontSize:35,fontWeight:FontWeight.bold )
+      body:SafeArea(child: Column(children:[ SizedBox(
+      height: 30, // <-- SEE HERE
+    ),
+    Container(width:double.infinity,
+        child: Text('   Welcome,\n   '+fname,style: TextStyle(fontSize:35,fontWeight:FontWeight.bold ),textAlign: TextAlign.left,
+        ),
       ),
-      Text(result),// Here the scanned result will be shown
-      ]
-      ),
-      ])
+      SizedBox(
+      height:30, // <-- SEE HERE
+    ),
+      //Text(result),// Here the scanned result will be shown
+      Container(
+                    margin: EdgeInsets.symmetric(horizontal: 32),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Color.fromRGBO(35, 60, 103, 1),
+                    ),
+                    padding: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          
+                          children: <Widget>[
+                            CircleAvatar(
+                              radius: 16,
+                              backgroundColor: Color.fromRGBO(50, 172, 121, 1),
+                              child: Icon(Icons.wallet_giftcard_sharp, color: Colors.white, size: 24,),
+                            ),
+                            
+                            
+                            Text("MAIN BALANCE", style: TextStyle(fontStyle: FontStyle.normal, fontSize: 28, color: Colors.white, fontWeight: FontWeight.w900),)
+                          ],
+                        ),
+                        SizedBox(height: 32,),
+                        
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [CircleAvatar(radius: 16,child: Icon(Icons.currency_rupee,color: Colors.white,),),
+                            Text(bal, style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.w700, letterSpacing: 2.0),),
+                          ],
+                        ),
+
+                        SizedBox(height: 32,),
+
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text("ADD MONEY", style: TextStyle(fontSize: 12, color: Colors.blue[100], fontWeight: FontWeight.w700, letterSpacing: 2.0),),
+                                IconButton(onPressed: (){}, icon: Icon(Icons.add_box),iconSize: 28,color: Colors.white,)
+                                //Text("Maaz Aftab", style: TextStyle(fontSize: 16, color: Colors.grey[100], fontWeight: FontWeight.w700, letterSpacing: 2.0),),
+                              ],
+                            ),
+                          ],
+                        )
+
+                      ],
+                    )
+                  ),]
+      )
       ),
       floatingActionButton: FloatingActionButton.extended(
           icon: Icon(Icons.camera_alt),
