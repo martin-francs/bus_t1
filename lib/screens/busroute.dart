42db0631c7129f996e12119686a58b98d3269bb9
Martin Francis Paul <francismartin0078@gmail.com>
2023-06-09 01:21:44 +0530

    dropdownlist added for bus routes

--- lib/screens/busroute.dart
+++ lib/screens/busroute.dart
@@ -11,62 +11,112 @@ class Busroutescreen extends StatefulWidget {
   _BusroutescreenState createState() => _BusroutescreenState();
 }
 
-class _BusroutescreenState extends State<Busroutescreen> {
-  String field1 = '';
-  //String documentId='KL33N9599';
-  String field2 = '';
+class _BusroutescreenState extends State<Busroutescreen>  {
+  Future<Map<String, dynamic>> _fetchMapFields() async {
+    try {
+      final DocumentSnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance
+          .collection('buses')
+          .doc(widget.documentId)
+          .get();
+
+      if (snapshot.exists) {
+        final Map<String, dynamic> mapFields = snapshot.data()!['STOPS'];
+        return mapFields;
+      } else {
+        throw Exception('Document does not exist');
+      }
+    } catch (e) {
+      throw Exception('Failed to fetch document: $e');
+    }
+  }
+
+  String _selectedOption = 'Placid';
+  String _selectedOption1 = 'Placid';
+  List<DropdownMenuItem<String>> _dropdownItems = [];
+  List<DropdownMenuItem<String>> _dropdownItems1 = [];
 
   @override
   void initState() {
     super.initState();
-    fetchDocumentFields(widget.documentId);
+    _fetchDropdownItems();
   }
 
-  Future<void> fetchDocumentFields(String documentId) async {
-    try {
-      DocumentSnapshot snapshot = await FirebaseFirestore.instance
-          .collection('buses')
-          .doc(documentId)
-          .get();
+  void _fetchDropdownItems() async {
+    final mapFields = await _fetchMapFields();
+    final List<String> options = mapFields.values.cast<String>().toList();
+    final List<String> options1 = mapFields.values.cast<String>().toList();
+    _selectedOption=options.first;
+    _selectedOption1=options1.first;
+    final List<DropdownMenuItem<String>> items = options
+        .map<DropdownMenuItem<String>>((value) => DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            ))
+        .toList();
+    final List<DropdownMenuItem<String>> items1 = options1
+        .map<DropdownMenuItem<String>>((value) => DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            ))
+        .toList();    
 
-      if (snapshot.exists) {
-        // Document exists, you can access its fields
-        Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
-
-        // Access specific fields
-        setState(() {
-          field1 = data['BUSNO'];
-          field2 = data['BusName'];
-         
-        });
-      } else {
-        // Document does not exist
-        print('Document does not exist');
-      }
-    } catch (e) {
-      // Error occurred while fetching document
-      print('Error fetching document: $e');
-    }
+    setState(() {
+      _dropdownItems = items;
+      _dropdownItems1 = items1;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BUS T'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('BUS T'),
+        ),
+        body: Column(
           children: [
-            Text('BUS no: $field1'),
-            Text('BUS NAME: $field2'),
-            Text(widget.documentId),
-          ],
+            Row(
+              children: [
+                Text("Starting Location"),
+                SizedBox(width: 50),
+                _dropdownItems.isEmpty
+                ? CircularProgressIndicator()
+                : DropdownButton<String>(
+                    value: _selectedOption,
+                    onChanged: (newValue) {
+                      setState(() {
+                        _selectedOption = newValue!;
+                      });
+                    },
+                    items: _dropdownItems,
+                  ),
+              ]
+            ),
+            
+          Row(
+              children: [
+                Text("Ending Location"),
+                SizedBox(width: 50),
+                _dropdownItems1.isEmpty
+                ? CircularProgressIndicator()
+                : DropdownButton<String>(
+                    value: _selectedOption1,
+                    onChanged: (newValue) {
+                      setState(() {
+                        _selectedOption1 = newValue!;
+                      });
+                    },
+                    items: _dropdownItems1,
+                  ),
+              ]
+            )],
+            
         ),
       ),
     );
   }
 }
+
+
 
 // Usage example
